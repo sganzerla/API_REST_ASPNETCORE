eaba66e820b3d802ec71cc7d4339fb489dc24b1a
Alisson Michel Sganzerla <alisson_sganzerla@live.com>
2018-12-05 20:41:00 -0200

    Implementação Soma

---
+++ 02 - Calculadora/API_REST_ASPNETCORE/API_REST_ASPNETCORE/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_ASPNETCORE.Controllers
+{
+    [Route("api/[controller]")]   
+    public class CalculadoraController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+           
+                return  BadRequest("Invalid Input");
+         
+        }
+
+        private Decimal ConvertToDecimal(string number)
+        {
+            decimal b;
+            if (Decimal.TryParse(number, out b))
+            {
+                return b;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string s)
+        {
+            float output;
+            return float.TryParse(s, out output);
+        }
+
+    }
+}
