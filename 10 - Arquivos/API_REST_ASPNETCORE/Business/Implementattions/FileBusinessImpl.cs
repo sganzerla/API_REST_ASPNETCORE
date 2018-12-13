6750a7cb2c829dcf48f4f0bafe82246fe83b2ebf
Alisson Michel Sganzerla <alisson_sganzerla@live.com>
2018-12-13 20:32:53 -0200

    Controller getPDF

---
+++ 10 - Arquivos/API_REST_ASPNETCORE/Business/Implementattions/FileBusinessImpl.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class FileBusinessImpl : IFileBusiness
+    {
+
+        public byte[] GetPDFFile()
+        {
+            string path = Directory.GetCurrentDirectory();
+            var fulPath = path + "\\Billing Management Console - Novembro.pdf";
+            return File.ReadAllBytes(fulPath);
+        }
+    }
+}
---
+++ 10 - Arquivos/API_REST_ASPNETCORE/Controllers/FileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Tapioca.HATEOAS;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class FileController : Controller
+    {
+        //Declaração do serviço usado
+        private IFileBusiness _fileBusiness;
+
+        /* Injeção de uma instancia de IBookBusiness ao criar
+        uma instancia de BookController */
+        public FileController(IFileBusiness fileBusiness)
+        {
+            _fileBusiness = fileBusiness;
+        }
+
+        [HttpGet]
+        [SwaggerResponse((200), Type = typeof(byte[]))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult GetPDFFile()
+        {
+            byte[] buffer = _fileBusiness.GetPDFFile();
+            if (buffer != null)
+            {
+                HttpContext.Response.ContentType = "application/pdf";
+                HttpContext.Response.Headers.Add("content-length", buffer.Length.ToString());
+                HttpContext.Response.Body.Write(buffer,0,buffer.Length);
+            }
+            return new ContentResult();
+        }
+    }
+}
